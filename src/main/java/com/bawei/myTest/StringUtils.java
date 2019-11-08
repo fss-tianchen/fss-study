46867232bf5787b9ca8b428f81d29a9dcf42f2a2
fss-tianchen <1036996714@qq.com>
2019-11-08 09:57:06 +0800

    判断是否有值

--- src/main/java/com/bawei/myTest/StringUtils.java
+++ src/main/java/com/bawei/myTest/StringUtils.java
@@ -6,4 +6,15 @@ public class StringUtils {
 	public static boolean notNull(String src) {
 		return (src == null)||(src.trim().equals(""));
 	}
+	
+	/**
+	 * @Title: haveValue 
+	 * @Description: 判断是否有值
+	 * @param str
+	 * @return
+	 * @return: boolean
+	 */
+	public static boolean haveValue(String str) {
+		return !(str==null||str.trim().equals(""));
+	}
 }
